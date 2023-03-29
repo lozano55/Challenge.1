e39e1e8d4f643d205e4450ff580ae3effe3989b9
lozano55 <119335160+lozano55@users.noreply.github.com>
2023-03-29 13:10:52 -0500

    Update, Part 1 Automate the Calculations. Complete

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -14,20 +14,29 @@ First, let's start with some calculations on a list of prices for 5 loans.
 """
 loan_costs = [500, 600, 200, 1000, 450]
 
+for loan in loan_costs:
+    
 # How many loans are in the list?
 # @TODO: Use the `len` function to calculate the total number of loans in the list.
 # Print the number of loans from the list
-# YOUR CODE HERE!
+    number_of_loans = len(loan_costs)
 
 # What is the total of all loans?
 # @TODO: Use the `sum` function to calculate the total of all loans in the list.
 # Print the total value of the loans
-# YOUR CODE HERE!
+    total_value = sum(loan_costs)
 
 # What is the average loan amount from the list?
 # @TODO: Using the sum of all loans and the total number of loans, calculate the average loan price.
 # Print the average loan amount
-# YOUR CODE HERE!
+    average_loan = total_value / number_of_loans
+    
+    loan_number = loan_number + 1
+    print(f"Loan: {loan_number} is a total of: ${loan}")
+    
+    
+print(f"The average Price per Loan is: ${average_loan:.2f}")
+
 
 """Part 2: Analyze Loan Data.
 
