1d52a0613e5aa61a707b822baaf54ff087649dee
lozano55 <119335160+lozano55@users.noreply.github.com>
2023-03-29 16:21:29 -0500

    Update, Part 4 Conditionally filter lists of loans. Completed

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -197,13 +197,19 @@ loans = [
 ]
 
 # @TODO: Create an empty list called `inexpensive_loans`
-# YOUR CODE HERE!
+inexpensive_loans = []
 
 # @TODO: Loop through all the loans and append any that cost $500 or less to the `inexpensive_loans` list
-# YOUR CODE HERE!
+for loan in loans:
+    if loan["loan_price"] <= 500:
+        inexpensive_loans.append(loan)
+        print(loan)
 
 # @TODO: Print the `inexpensive_loans` list
-# YOUR CODE HERE!
+print()
+print(inexpensive_loans)
+
+
 
 
 """Part 5: Save the results.
