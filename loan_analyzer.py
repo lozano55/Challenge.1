7487a51af71c337f013aa8e23815b4328b13fc2f
lozano55 <119335160+lozano55@users.noreply.github.com>
2023-03-29 16:41:11 -0500

    Update, I am officially Done

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -226,6 +226,11 @@ Output this list of inexpensive loans to a csv file
 
 """
 
+
+import csv
+
+from pathlib import Path
+
 # Set the output header
 header = ["loan_price", "remaining_months", "repayment_interval", "future_value"]
 
@@ -234,4 +239,10 @@ output_path = Path("inexpensive_loans.csv")
 
 # @TODO: Use the csv library and `csv.writer` to write the header row
 # and each row of `loan.values()` from the `inexpensive_loans` list.
-# YOUR CODE HERE!
+
+with open(output_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(header)
+    
+    for loan in inexpensive_loans:
+        csvwriter.writerow(loan.values())
